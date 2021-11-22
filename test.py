f43b50f3499ee567df49e89c8cbc7a690bcc5ee6
Sadra Barikbin <sadra.barikbin@ce.sharif.edu>
2021-11-23 02:11:54 +0330

    Fix bug in test.py and some testcases

--- test.py
+++ test.py
@@ -10,7 +10,9 @@ class VerseExtractionTest(TestCase):
         tests_dir=Path('tests')
         for i in range(1,len(list((tests_dir / 'in').iterdir()))+1):
             input=(tests_dir / 'in' / f'input{i}.txt').read_text(encoding="utf-8")
-            output=(tests_dir / 'out' / f'output{i}.txt').read_text(encoding="utf-8").split()
+            output=(tests_dir / 'out' / f'output{i}.txt').read_text(encoding="utf-8").split('\n')
+            if output==['']:
+                output=[]
             with self.subTest(test=f"Test#{i}"):
                 self.assertListEqual(ayeh_extractor(input), output)
 

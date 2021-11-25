26a8c60590db2286914fd5fcdfa4b13b5ec23d7f
Seyed Mohammad Hosseini <semohosseini@outlook.com>
2021-11-25 17:55:26 +0330

    تغییرات جزیی در تست‌ها

--- test.py
+++ test.py
@@ -4,17 +4,22 @@ import unittest
 
 from quran import ayeh_extractor
 
+
 class VerseExtractionTest(TestCase):
 
     def test_extraction(self):
-        tests_dir=Path('tests')
-        for i in range(1,len(list((tests_dir / 'in').iterdir()))+1):
-            input=(tests_dir / 'in' / f'input{i}.txt').read_text(encoding="utf-8")
-            output=(tests_dir / 'out' / f'output{i}.txt').read_text(encoding="utf-8").split('\n')
-            if output==['']:
-                output=[]
+        tests_dir = Path('tests')
+        for i in range(1, len(list((tests_dir / 'in').iterdir())) + 1):
+            text_in = (tests_dir / 'in' / f'input{i}.txt').read_text(encoding="utf-8")
+            test_out = (tests_dir / 'out' / f'output{i}.txt').read_text(encoding="utf-8").strip().split('\n')
+            if test_out == ['']:
+                test_out = []
             with self.subTest(test=f"Test#{i}"):
-                self.assertListEqual(ayeh_extractor(input), output)
+                func_out = ayeh_extractor(text_in)
+                print('A:', func_out)
+                print('B:', test_out)
+
+                self.assertListEqual(func_out, test_out)
 
 
 if __name__ == "__main__":
